4fb26eed1acd5fd45938d336cd0752a8f50f61f2
root <root@OSC>
2018-01-24 20:44:05 -0700

    Q1 done (maybe)

--- assign1/q1.c
+++ assign1/q1.c
@@ -5,11 +5,20 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/sched.h>
+#include <linux/types.h>
 
-void print_init_PCB(void) {
-  int pid = init_task.pid;
-  long state = init_task.state;
-  printk(KERN_INFO "name: %s, state: %ld, pid:%d", init_task.comm, state, pid);
+void print_dfs(struct task_struct *task) {
+  printk(KERN_INFO "name: %s, state: %ld, pid:%d", task->comm, task->state, task->pid); // print this task
+  
+  struct task_struct *child;
+  struct list_head *list;
+  
+  // list is the pointer to the current element
+  // task->chilren is the head of the list
+  list_for_each(list, &task-> children) {
+    child = list_entry(list, struct task_struct, sibling);
+    print_dfs(child);
+  }
   
 }
 
@@ -17,7 +26,7 @@ void print_init_PCB(void) {
 /* This function is called when the module is loaded. */
 int simple_init(void)
 { printk(KERN_INFO "Loading Module\n");
-  print_init_PCB();
+  print_dfs(&init_task);
   return 0;
 }
 
