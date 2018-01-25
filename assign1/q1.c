7d6372f4fe375152b7f5cf3d7b9f5bb5e153fa91
gadriwau <gadriwau@mcmaster.ca>
2018-01-24 20:18:58 -0700

    Started Q1

--- assign1/q1.c
+++ assign1/q1.c
@@ -1,3 +1,35 @@
-C
-C
+// Tasnim Noshin
+// Umme Salma Gadriwala
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+
+void print_init_PCB(void) {
+  int pid = init_task.pid;
+  long state = init_task.state;
+  printk(KERN_INFO "name: %s, state: %ld, pid:%d", init_task.comm, state, pid);
+  
+}
+
+
+/* This function is called when the module is loaded. */
+int simple_init(void)
+{ printk(KERN_INFO "Loading Module\n");
+  print_init_PCB();
+  return 0;
+}
+
+/* This function is called when the module is removed. */
+void simple_exit(void)
+{ printk(KERN_INFO "Removing Module\n");
+}
+/* Macros for registering module entry and exit points. */
+module_init(simple_init);
+module_exit(simple_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Simple Module");
+MODULE_AUTHOR("SGG");
+
 
